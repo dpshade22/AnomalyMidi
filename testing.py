22e438d268cf982ebad6451b898a7f3c8c10f669
Dylan Shade <dpshadey22@gmail.com>
2023-03-25 12:41:30 -0400

    fixed testing.py

--- testing.py
+++ testing.py
@@ -17,29 +17,35 @@ def precision(y_true, y_pred):
 # Register the custom metric function with Keras
 tf.keras.metrics.Precision = precision
 # Replace 'model_file.h5' with the name of your H5 file
-model_file = "model.h5"
+model_file = "./model/model.h5"
 
 # Load the model from the H5 file
 model = load_model(model_file)
 
 
 def predict_anomalies(model, file, sequence_length=500):
-    df = pd.read_csv(file)
+    df = pd.read_csv(file, usecols=[1, 2, 3], index_col=False)
+    print(df.columns)
     scaler = MinMaxScaler()
     df[["note", "velocity", "time"]] = scaler.fit_transform(
         df[["note", "velocity", "time"]]
     )
 
-    input_data = []
-    for i in range(len(df) - sequence_length):
-        input_data.append(df.iloc[i : i + sequence_length, :-1].values)
+    # Pad the input data if the number of notes is less than the sequence length
+    if len(df) < sequence_length:
+        padding = pd.DataFrame(
+            np.zeros((sequence_length - len(df), 3)),
+            columns=["note", "velocity", "time"],
+        )
+        df = pd.concat([df, padding], ignore_index=True)
 
-    input_data = np.array(input_data)
+    input_data = df.iloc[:sequence_length, :].values.reshape(1, sequence_length, -1)
+    print(input_data.shape)
     predictions = model.predict(input_data)
     predictions[predictions < 0] = 0
     return int(np.round(np.sum(predictions)))
 
 
-new_file = "anomalous/alb_esp1_modified01.csv"
+new_file = "anomalous/waldstein_3_modified0.csv"
 num_anomalies = predict_anomalies(model, new_file)
 print(f"Number of anomalies: {num_anomalies}")
